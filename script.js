14ede8b38a45e2a48655cd8a82945ea9e4c1a9f0
Jyoti-coder1 <jyotimathsdev@gmail.com>
2026-04-27 18:46:29 +0530

    feat: implement add task functionality (#3)

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// JS will be added in next issues
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+
+// Array to store tasks
+let tasks = [];
+
+// Render function
+function renderTasks() {
+    taskList.innerHTML = "";
+
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+        li.textContent = task;
+        taskList.appendChild(li);
+    });
+}
+
+// Add Task Function
+function addTask() {
+    const text = taskInput.value.trim();
+
+    // Prevent empty input
+    if (!text) {
+        alert("Enter task!");
+        return;
+    }
+
+    // Add to array
+    tasks.push(text);
+
+    // Clear input field
+    taskInput.value = "";
+
+    // Trigger render function to update UI
+    renderTasks();
+}
+
+// Button click
+addTaskBtn.addEventListener("click", addTask);
